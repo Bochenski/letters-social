54f376a81722bdd94a81eb03457ccf3aa171bf60
David Bochenski <david@goincremental.com>
2020-03-14 21:57:47 +0000

    Chapter 5

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import parseLinkHeader from 'parse-link-header';
 import orderBy from 'lodash/orderBy';
@@ -10,6 +10,7 @@ import Loader from './components/Loader';
 import * as API from './shared/http';
 import Ad from './components/ad/Ad';
 import Post from './components/post/Post';
+import CreatePost from './components/post/Create';
 import Welcome from './components/welcome/Welcome';
 
 /**
@@ -25,13 +26,14 @@ class App extends Component {
             error: null,
             loading: false,
             posts: [],
-            endpoint: `${process.env
-                .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
+            endpoint: `${process.env.ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`
         };
-        this.getPosts = this.getPosts.bind(this);
+        this.getPosts = this
+            .getPosts
+            .bind(this);
     }
     static propTypes = {
-        children: PropTypes.node,
+        children: PropTypes.node
     };
     componentDidMount() {
         this.getPosts();
@@ -39,67 +41,73 @@ class App extends Component {
     componentDidCatch(err, info) {
         console.error(err);
         console.error(info);
-        this.setState(() => ({
-            error: err,
-        }));
+        this.setState(() => ({error: err}));
     }
     getPosts() {
-        API.fetchPosts(this.state.endpoint)
+        API
+            .fetchPosts(this.state.endpoint)
             .then(res => {
-                return res.json().then(posts => {
-                    const links = parseLinkHeader(res.headers.get('Link'));
-                    this.setState(() => ({
-                        posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                        endpoint: links.next.url,
-                    }));
-                });
+                return res
+                    .json()
+                    .then(posts => {
+                        const links = parseLinkHeader(res.headers.get('Link'));
+                        this.setState(() => ({
+                            posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
+                            endpoint: links.next.url
+                        }));
+                    });
             })
             .catch(err => {
-                this.setState(() => ({ error: err }));
+                this.setState(() => ({error: err}));
             });
+    }
+    createNewPost(post) {
+        this.setState(prevState => {
+            return {
+                posts: orderBy(prevState.posts.concat(newPost), 'date', 'desc')
+            };
+        });
     }
     render() {
         if (this.state.error) {
             return (
                 <div className="app">
-                    <ErrorMessage error={this.state.error} />
+                    <ErrorMessage error={this.state.error}/>
                 </div>
             );
         }
         return (
             <div className="app">
-                <Nav user={this.props.user} />
-                {this.state.loading ? (
-                    <div className="loading">
-                        <Loader />
-                    </div>
-                ) : (
-                    <div className="home">
-                        <Welcome key="welcome" />
-                        <div>
-                            {this.state.posts.length && (
-                                <div className="posts">
-                                    {this.state.posts.map(({ id }) => {
-                                        return <Post id={id} key={id} user={this.props.user} />;
-                                    })}
-                                </div>
-                            )}
-                            <button className="block" onClick={this.getPosts}>
-                                Load more posts
-                            </button>
+                <Nav user={this.props.user}/> {this.state.loading
+                    ? (
+                        <div className="loading">
+                            <Loader/>
                         </div>
-                        <div>
-                            <Ad
-                                url="https://ifelse.io/book"
-                                imageUrl="/static/assets/ads/ria.png"
-                            />
-                            <Ad
-                                url="https://ifelse.io/book"
-                                imageUrl="/static/assets/ads/orly.jpg"
-                            />
+                    )
+                    : (
+                        <div className="home">
+                            <Welcome key="welcome"/>
+                            <div>
+                                <CreatePost onSubmit={this.createNewPost}/> {this.state.posts.length && (
+                                    <div className="posts">
+                                        {this
+                                            .state
+                                            .posts
+                                            .map(({id}) => {
+                                                return <Post id={id} key={id} user={this.props.user}/>;
+                                            })}
+                                    </div>
+                                )}
+                                <button className="block" onClick={this.getPosts}>
+                                    Load more posts
+                                </button>
+                            </div>
+                            <div>
+                                <Ad url="https://ifelse.io/book" imageUrl="/static/assets/ads/ria.png"/>
+                                <Ad url="https://ifelse.io/book" imageUrl="/static/assets/ads/orly.jpg"/>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
             </div>
         );
     }
