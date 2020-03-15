dfc6262c5b386a730ef713a90f0666b0b1ffa259
David Bochenski <david@goincremental.com>
2020-03-15 17:41:34 +0000

    chapter 5

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import parseLinkHeader from 'parse-link-header';
-import orderBy from 'lodash/orderBy';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import parseLinkHeader from "parse-link-header";
+import orderBy from "lodash/orderBy";
 
-import ErrorMessage from './components/error/Error';
-import Nav from './components/nav/navbar';
-import Loader from './components/Loader';
+import ErrorMessage from "./components/error/Error";
+import Nav from "./components/nav/navbar";
+import Loader from "./components/Loader";
 
-import * as API from './shared/http';
-import Ad from './components/ad/Ad';
-import Post from './components/post/Post';
-import CreatePost from './components/post/Create';
-import Welcome from './components/welcome/Welcome';
+import * as API from "./shared/http";
+import Ad from "./components/ad/Ad";
+import Post from "./components/post/Post";
+import CreatePost from "./components/post/Create";
+import Welcome from "./components/welcome/Welcome";
 
 /**
  * The app component serves as a root for the project and renders either children,
@@ -26,11 +26,12 @@ class App extends Component {
             error: null,
             loading: false,
             posts: [],
-            endpoint: `${process.env.ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`
+            endpoint: `${
+                process.env.ENDPOINT
+            }/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`
         };
-        this.getPosts = this
-            .getPosts
-            .bind(this);
+        this.getPosts = this.getPosts.bind(this);
+        this.createNewPost = this.createNewPost.bind(this);
     }
     static propTypes = {
         children: PropTypes.node
@@ -41,73 +42,94 @@ class App extends Component {
     componentDidCatch(err, info) {
         console.error(err);
         console.error(info);
-        this.setState(() => ({error: err}));
+        this.setState(() => ({ error: err }));
     }
     getPosts() {
-        API
-            .fetchPosts(this.state.endpoint)
+        API.fetchPosts(this.state.endpoint)
             .then(res => {
-                return res
-                    .json()
-                    .then(posts => {
-                        const links = parseLinkHeader(res.headers.get('Link'));
-                        this.setState(() => ({
-                            posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                            endpoint: links.next.url
-                        }));
-                    });
+                return res.json().then(posts => {
+                    const links = parseLinkHeader(res.headers.get("Link"));
+                    this.setState(() => ({
+                        posts: orderBy(
+                            this.state.posts.concat(posts),
+                            "date",
+                            "desc"
+                        ),
+                        endpoint: links.next.url
+                    }));
+                });
             })
             .catch(err => {
-                this.setState(() => ({error: err}));
+                this.setState(() => ({ error: err }));
             });
     }
     createNewPost(post) {
-        this.setState(prevState => {
-            return {
-                posts: orderBy(prevState.posts.concat(newPost), 'date', 'desc')
-            };
-        });
+        return API.createPost(post)
+            .then(res => res.json())
+            .then(newPost => {
+                this.setState(prevState => {
+                    return {
+                        posts: orderBy(
+                            prevState.posts.concat(newPost),
+                            "date",
+                            "desc"
+                        )
+                    };
+                });
+            })
+            .catch(err => {
+                this.setState(() => ({ error: err }));
+            });
     }
     render() {
         if (this.state.error) {
             return (
                 <div className="app">
-                    <ErrorMessage error={this.state.error}/>
+                    <ErrorMessage error={this.state.error} />
                 </div>
             );
         }
         return (
             <div className="app">
-                <Nav user={this.props.user}/> {this.state.loading
-                    ? (
-                        <div className="loading">
-                            <Loader/>
+                <Nav user={this.props.user} />{" "}
+                {this.state.loading ? (
+                    <div className="loading">
+                        <Loader />
+                    </div>
+                ) : (
+                    <div className="home">
+                        <Welcome key="welcome" />
+                        <div>
+                            <CreatePost onSubmit={this.createNewPost} />{" "}
+                            {this.state.posts.length && (
+                                <div className="posts">
+                                    {this.state.posts.map(({ id }) => {
+                                        return (
+                                            <Post
+                                                id={id}
+                                                key={id}
+                                                user={this.props.user}
+                                            />
+                                        );
+                                    })}
+                                </div>
+                            )}
+                            <button className="block" onClick={this.getPosts}>
+                                Load more posts
+                            </button>
                         </div>
-                    )
-                    : (
-                        <div className="home">
-                            <Welcome key="welcome"/>
-                            <div>
-                                <CreatePost onSubmit={this.createNewPost}/> {this.state.posts.length && (
-                                    <div className="posts">
-                                        {this
-                                            .state
-                                            .posts
-                                            .map(({id}) => {
-                                                return <Post id={id} key={id} user={this.props.user}/>;
-                                            })}
-                                    </div>
-                                )}
-                                <button className="block" onClick={this.getPosts}>
-                                    Load more posts
-                                </button>
-                            </div>
-                            <div>
-                                <Ad url="https://ifelse.io/book" imageUrl="/static/assets/ads/ria.png"/>
-                                <Ad url="https://ifelse.io/book" imageUrl="/static/assets/ads/orly.jpg"/>
-                            </div>
+                        <div>
+                            <Ad
+                                url="https://ifelse.io/book"
+                                imageUrl="/static/assets/ads/ria.png"
+                            />
+                            <Ad
+                                url="https://ifelse.io/book"
+                                imageUrl="/static/assets/ads/orly.jpg"
+                            />
                         </div>
-                    )}
+                    </div>
+                )}
             </div>
         );
     }
--- src/components/post/Create.js
+++ src/components/post/Create.js
@@ -1,61 +1,159 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import Filter from 'bad-words';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Filter from "bad-words";
+import classnames from "classnames";
+
+import LocationTypeAhead from "../map/LocationTypeAhead";
+import DisplayMap from "../map/DisplayMap";
 
 const filter = new Filter();
 
 class CreatePost extends Component {
-  static propTypes = {}
-  constructor(props) {
-    super(props);
+    static propTypes = {};
+    constructor(props) {
+        super(props);
 
-    //set up state
-    this.state = {
-      content: '',
-      valid: false
-    };
+        //set up state
+        this.initialState = {
+            content: "",
+            valid: false,
+            showLocationPicker: false,
+            location: {
+                lat: 34.1535641,
+                lng: -118.1428115,
+                name: null
+            },
+            locationSelected: false
+        };
 
-    //set up handlers
-    this.handleSubmit = this
-      .handleSubmit
-      .bind(this);
-    this.handlePostChange = this
-      .handlePostChange
-      .bind(this);
-  }
-
-  handlePostChange(event) {
-    const content = filter.clean(event.target.value);
-    this.setState(() => {
-      return {
-        content,
-        valid: content.length <= 280
-      };
-    });
-  }
-
-  handleSubmit() {
-    if (!this.state.valid) {
-      return;
+        this.state = this.initialState;
+        this.filter = new Filter();
+        //set up handlers
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRemoveLocation = this.handleRemoveLocation.bind(this);
+        this.handleToggleLocation = this.handleToggleLocation.bind(this);
+        this.handlePostChange = this.handlePostChange.bind(this);
+        this.onLocationSelect = this.onLocationSelect.bind(this);
+        this.onLocationUpdate = this.onLocationUpdate.bind(this);
+        this.renderLocationControls = this.renderLocationControls.bind(this);
     }
-    const newPost = {
-      content: this.state.content
-    };
+    handleRemoveLocation() {
+        this.setState(() => ({
+            locationSelected: false,
+            location: this.initialState.location
+        }));
+    }
+    handlePostChange(event) {
+        const content = filter.clean(event.target.value);
+        this.setState(() => {
+            return {
+                content,
+                valid: content.length <= 280
+            };
+        });
+    }
 
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <div classname="create-post">
-        <button onClick={this.handleSubmit}>Post</button>
-        <textarea
-          onChange={this.handlePostChange}
-          placeholder="What's on your mind?"
-          value={this.state.content}/>
-      </div>
-    );
-  }
+    handleSubmit() {
+        if (!this.state.valid) {
+            return;
+        }
+        const newPost = {
+            content: this.state.content
+        };
+        if (this.state.locationSelected) {
+            newPost.location = this.state.location;
+        }
+        this.props.onSubmit(newPost);
+        this.setState(() => ({
+            content: "",
+            valid: false,
+            showLocationPicker: false,
+            location: this.initialState.location,
+            locationSelected: false
+        }));
+    }
+    onLocationUpdate(location) {
+        this.setState(() => ({ location }));
+    }
+    onLocationSelect(location) {
+        this.setState(() => ({
+            location,
+            showLocationPicker: false,
+            locationSelected: true
+        }));
+    }
+    handleToggleLocation(e) {
+        e.preventDefault();
+        this.setState(state => ({
+            showLocationPicker: !state.showLocationPicker
+        }));
+    }
+    renderLocationControls() {
+        return (
+            <div className="controls">
+                <button onClick={this.handleSubmit}>Post</button>
+                {this.state.location && this.state.locationSelected ? (
+                    <button
+                        onClick={this.handleRemoveLocation}
+                        className="open location-indicator"
+                    >
+                        <i className="fa-location-arrow fa" />
+                        <small>{this.state.location.name}</small>
+                    </button>
+                ) : (
+                    <button
+                        onClick={this.handleToggleLocation}
+                        className="open"
+                    >
+                        {this.state.showLocationPicker
+                            ? "Cancel"
+                            : "Add location"}{" "}
+                        <i
+                            className={classnames(`fa`, {
+                                "fa-map-o": !this.state.showLocationPicker,
+                                "fa-times": this.state.showLocationPicker
+                            })}
+                        />
+                    </button>
+                )}
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div className="create-post">
+                <textarea
+                    onChange={this.handlePostChange}
+                    placeholder="What's on your mind?"
+                    value={this.state.content}
+                />
+                {this.renderLocationControls()}
+                <div
+                    className="location-picker"
+                    style={{
+                        display: this.state.showLocationPicker
+                            ? "block"
+                            : "none"
+                    }}
+                >
+                    {!this.state.locationSelected && (
+                        <LocationTypeAhead
+                            key="LocationTypeAhead"
+                            onLocationSelect={this.onLocationSelect}
+                            onLocationUpdate={this.onLocationUpdate}
+                        />
+                    )}
+                    <DisplayMap
+                        key="DisplayMap"
+                        displayOnly={false}
+                        location={this.state.location}
+                        onLocationSelect={this.onLocationSelect}
+                        onLocationUpdate={this.onLocationUpdate}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 ////
 export default CreatePost;
